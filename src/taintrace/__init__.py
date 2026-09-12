ba40bedfb8c7de51d7a4e4a68488f285055eaf0f
Yunare Maia <yunare@gmail.com>
2026-09-12 18:20:33 +0000

    chore: sync version to v0.2.0

--- src/taintrace/__init__.py
+++ src/taintrace/__init__.py
@@ -6,7 +6,7 @@ from taintrace.db import KnownPackagesDB
 from taintrace.scorer import RiskScorer, RiskResult, RiskLevel
 from taintrace.detector import TyposquatDetector, DetectionResult
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 __all__ = [
     "LockfileParser",
